a835928b5bfa9f9a0e85c648faa9e773a1db4fb4
silent-cloud <johnlorenzsalva@gmail.com>
2022-02-23 13:13:59 -0800

    Changed from fetch to axios to perform api calls with error catches, changed text input to functionality to be automatic and simpler

--- App.js
+++ App.js
@@ -1,38 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, TextInput, View } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import { setString as cb } from 'expo-clipboard';
+import axios from 'axios';
 
 export default function App() {
+  const [ urlSubmitted, setUrlSubmitted ] = useState('');
   const [ tUrl, setTUrl ] = useState('');
   const [ showBtn, setShowBtn ] = useState(false);
   const { control, handleSubmit, formState: {errors, isValid}, reset } = useForm({mode: 'onBlur'});
   const onSubmit = data => {
-    console.log(data.inputUrl)
-    if (data.inputUrl !== '') {
-      fetch('https://api.tinyurl.com/create', 
-        {
-          headers: {
-            Accept: 'application/json',
-            Authorization: 'Bearer <token>',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'url': data.inputUrl
-          }),
-          method: 'POST',
-        },
-      ).then((response) => response.json())
-      .then((responseJson) => {
-        setTUrl(responseJson.data.tiny_url);
-        setShowBtn(true);
-      })
+    setUrlSubmitted(data.inputUrl);
+  };
+
+  useEffect(() => {
+    if (urlSubmitted !== '') {
+      const fetch = async () => {
+        try {
+          await axios.post('https://api.tinyurl.com/create', 
+            {
+              'url': urlSubmitted
+            },
+            {
+              headers: {
+                Accept: 'application/json',
+                Authorization: 'Bearer <token>',
+                'Content-Type': 'application/json'
+              }
+            }
+          ).then((response) => {
+            setTUrl(response.data.data.tiny_url);
+            setShowBtn(true);
+          })
+        } catch (e) {
+          console.error(e.message);
+        }
+      }
+      fetch();
     } else {
       setTUrl('');
       setShowBtn(false);
     }
-  };
+  });
 
   return (
     <View style={styles.container}>
@@ -47,6 +57,8 @@ export default function App() {
             value={value}
             onblur={onBlur}
             onChangeText={value => onChange(value)}
+            keyboardType='email-address'
+            onEndEditing={handleSubmit(onSubmit)}
           />
         )}
       />
@@ -58,10 +70,11 @@ export default function App() {
             <Text style={styles.shortUrlButtonText}>{tUrl}</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.shortUrlButton} onPress={() => {
+            setUrlSubmitted('')
             setTUrl('');
             setShowBtn(false);
             reset({
-              inputURL: ''
+              inputUrl: ''
             });
           }}>
             <Text style={styles.shortUrlButtonText}>Clear</Text>
@@ -91,7 +104,10 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderRadius: 4,
     margin: 10,
-    padding: 5
+    paddingTop: 5,
+    paddingLeft: 10,
+    paddingBottom: 5,
+    fontSize: 20
   },
   screen: {
     paddingTop: 70,
